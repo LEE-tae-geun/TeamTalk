43dc191741439ba7d588b5e7d7754baa5adb421d
hjs7115 <hjs7115@gmail.com>
2025-11-07 22:15:56 +0900

    메인 페이지

--- src/main/java/com/example/chat_server/controller/PageController.java
+++ src/main/java/com/example/chat_server/controller/PageController.java
@@ -8,8 +8,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PageController {
 
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "index";
     }
     //페이지를 조회 및 이동할때 위와같이 @GetMapping()을 써서 이동.
+
+    @GetMapping("/register")
+    public String register() {
+        return "register/index";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "login/index";
+    }
 }
